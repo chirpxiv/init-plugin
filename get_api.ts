6a001a7bea42c57faa5030f88639031fd3777ce5
chirp <ch1rp@protonmail.com>
2022-10-01 16:43:25 +0100

    Write script to fetch all services from Dalamud repo

---
+++ get_api.ts
@@ -0,0 +1,80 @@
+// Dependencies
+
+import { ensureDirSync } from "https://deno.land/std/fs/mod.ts";
+
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest?dts";
+
+// Constants
+
+const repo = "goatcorp/Dalamud";
+
+const outputDir = "./data";
+const outputFile = "api.json";
+
+// API Code
+
+const octokit = new Octokit();
+
+async function getServices() {
+	const results = [];
+
+	const iterator = await octokit.paginate.iterator(octokit.rest.search.code, {
+		q: `[PluginInterface]+in:file+language:cs+repo:${repo}`,
+		per_page: 100
+	});
+
+	for await (const {data:items} of iterator) {
+		const pageResult = items
+		.map((item: {path:string;}) => {
+			// Despite specifying the language in the query, GitHub still returns HTML pages.
+			if (!item.path.startsWith("Dalamud/") || !item.path.endsWith(".cs"))
+				return null;
+			
+			const path = item.path.split("/"),
+				namespace = path.slice(0, -1).join("."),
+				className = path.pop()?.split(".")[0];
+			
+			return {
+				path: item.path,
+				namespace,
+				className
+			};
+		})
+		.filter((item: {}) => item != null);
+
+		results.push(...pageResult);
+	}
+	
+	return results;
+}
+
+// File exists
+
+function exists(path: string) {
+	try {
+		return Deno.lstatSync(path).isFile;
+	} catch {
+		return false;
+	}
+}
+
+// Main
+
+async function main() {
+	const outputPath = `${outputDir}/${outputFile}`;
+
+	ensureDirSync(outputDir);
+
+	if (exists(outputPath))
+		return;
+
+	const api = {
+		services: await getServices()
+	};
+
+	Deno.writeTextFileSync(outputPath, JSON.stringify(api, null, 4));
+}
+
+await main();
+
+Deno.exit();
--- src/interface.rs
+++ src/interface.rs
@@ -1,33 +1,126 @@
 // Dependencies
 
-use egui::Ui;
+use egui::{Ui, RichText};
 use eframe::egui;
 
-// Current tab
+// Constants
+
+static LIBRARIES: &'static [(&str, bool)] = &[ // name, enabled
+	("FFXIVClientStructs", true),
+	("Dalamud", true),
+	("ImGui.NET", true),
+	("ImGuiScene", true),
+	("Lumina", true),
+	("Lumina.Excel", true),
+	("SharpDX", false),
+	("SharpDX.Mathematics", false),
+	("Newtonsoft.Json", false),
+	("CheapLoc", false)
+];
+
+// Tab enum
 
 #[derive(PartialEq)]
 pub enum Tab {
 	Basics,
-	Dependencies
+	Dependencies,
+	Services
 }
 
 // Gui
 
 pub struct Gui {
-	pub tab: Tab
+	pub tab: Tab,
+
+	pub name: String,
+	pub name_internal: String,
+	pub description: String,
+	pub author: String,
+	pub tags: String,
+
+	pub libraries: Vec<(String, bool)>, // name, enabled
+
+	pub service_class: String
 }
 
 impl Gui {
 	pub fn new() -> Self {
-		Self {
-			tab: Tab::Basics
+		let mut libraries = Vec::new();
+
+		for pair in LIBRARIES {
+			libraries.push((pair.0.to_string(), pair.1));
 		}
+
+		Self {
+			tab: Tab::Basics,
+
+			name: "".to_string(),
+			name_internal: "".to_string(),
+			description: "".to_string(),
+			author: "".to_string(),
+			tags: "".to_string(),
+
+			service_class: "Services".to_string(),
+
+			libraries
+		}
+	}
+
+	// UI helpers
+
+	pub fn heading(ui: &mut Ui, text: &str) {
+		ui.label(RichText::new(text).heading().strong());
+	}
+
+	pub fn bold(ui: &mut Ui, text: &str) {
+		ui.label(RichText::new(text).strong());
 	}
 
 	// Tabs
 
 	fn tab_basics(&mut self, ui: &mut Ui) {
-		
+		Gui::heading(ui, "Basics");
+
+		ui.separator();
+
+		Gui::bold(ui, "Name");
+		ui.label("The plugin's title. This will be shown in the plugin installer.");
+		ui.text_edit_singleline(&mut self.name);
+
+		Gui::bold(ui, "Internal Name");
+		ui.label("This will be the plugin's namespace.");
+		ui.text_edit_singleline(&mut self.name_internal);
+
+		Gui::bold(ui, "Description");
+		ui.text_edit_multiline(&mut self.description);
+
+		ui.separator();
+
+		Gui::bold(ui, "Author");
+		ui.label("That's you.");
+		ui.text_edit_singleline(&mut self.author);
+
+		Gui::bold(ui, "Tags");
+		ui.text_edit_multiline(&mut self.tags);
+	}
+
+	fn tab_deps(&mut self, ui: &mut Ui) {
+		Gui::heading(ui, "Dependencies");
+
+		ui.separator();
+
+		for pair in &mut self.libraries {
+			ui.checkbox(&mut pair.1, pair.0.to_owned());
+		}
+	}
+
+	fn tab_services(&mut self, ui: &mut Ui) {
+		Gui::heading(ui, "Services");
+
+		ui.separator();
+
+		Gui::bold(ui, "Class Name");
+		ui.text_edit_singleline(&mut self.service_class);
 	}
 }
 
@@ -46,16 +139,22 @@ impl eframe::App for Gui {
 				{ self.tab = Tab::Basics; }
 			if ui.selectable_label(self.tab == Tab::Dependencies, "Dependencies").clicked()
 				{ self.tab = Tab::Dependencies; }
+			if ui.selectable_label(self.tab == Tab::Services, "Services").clicked()
+				{ self.tab = Tab::Services; }
 		});
 
 		// Draw tab
 
 		egui::CentralPanel::default().show(ctx, |ui| {
+			let spacing = ui.spacing_mut();
+			spacing.item_spacing.y = 10.0;
+
 			match self.tab {
 				Tab::Basics => self.tab_basics(ui),
+				Tab::Dependencies => self.tab_deps(ui),
+				Tab::Services => self.tab_services(ui),
 				_ => {}
 			}
 		});
 	}
-
 }
